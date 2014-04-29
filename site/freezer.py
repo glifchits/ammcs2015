42094155b85ecd5aa511c4ead656a229ae3d6029
George Lifchits <george.lifchits@gmail.com>
2014-04-29 19:24:01 -0400

    generator for special sessions

--- site/freezer.py
+++ site/freezer.py
@@ -1,12 +1,17 @@
 from flask_frozen import Freezer
-from main import app, ROUTES
+from main import app, ROUTES, SESSIONS
 
 freezer = Freezer(app)
 
 @freezer.register_generator
-def routing():
+def root_routing():
     for route in ROUTES.itervalues():
         yield {"route": route}
+
+@freezer.register_generator
+def special_sessions():
+    for session in SESSIONS.keys():
+        yield {"session": session}
 
 if __name__ == "__main__":
     freezer.run(debug=True)
