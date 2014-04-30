04fd192f1b12a55d95a959031c7430f7931f51ef
George Lifchits <george.lifchits@gmail.com>
2014-04-30 02:00:35 -0400

    updated freezer

--- site/freezer.py
+++ site/freezer.py
@@ -1,17 +1,12 @@
 from flask_frozen import Freezer
-from main import app, ROUTES, SESSIONS
+from main import app, PAGES
 
 freezer = Freezer(app)
 
 @freezer.register_generator
-def root_routing():
-    for route in ROUTES.itervalues():
+def routing():
+    for route in PAGES:
         yield {"route": route}
-
-@freezer.register_generator
-def special_sessions():
-    for session in SESSIONS.keys():
-        yield {"session": session}
 
 if __name__ == "__main__":
     freezer.run(debug=True)
