bbecc1ba7167fc69df496462243289571a3d9999
George Lifchits <george.lifchits@gmail.com>
2014-05-03 14:32:54 -0400

    freezer now removes old build, starts simplehttpserver

--- site/freezer.py
+++ site/freezer.py
@@ -1,5 +1,7 @@
 from flask_frozen import Freezer
 from main import app, PAGES
+import shutil
+os = shutil.os
 
 freezer = Freezer(app)
 
@@ -9,4 +11,22 @@ def routing():
         yield {"route": route}
 
 if __name__ == "__main__":
-    freezer.run(debug=True)
+    if os.path.exists('build'):
+        shutil.rmtree('build')
+    freezer.freeze()
+    oldcwd = os.getcwd()
+    os.chdir('build')
+    import SimpleHTTPServer, SocketServer
+    PORT = 8000
+    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    httpd = SocketServer.TCPServer(("", PORT), Handler)
+    try:
+        print "serving at port", PORT
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print 'caught KeyboardInterrupt'
+        raise
+    finally:
+        print 'shutting down'
+        httpd.shutdown()
+        os.chdir(oldcwd)
