b56ff0501a108a09abe1d610bf1c11e9946f4b0d
George Lifchits <george.lifchits@gmail.com>
2014-05-05 17:01:16 -0400

    deploy builds first, optionally skips non-html files

--- site/deploy.sh
+++ site/deploy.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-scp -vr build/* lifc1350@cubic.wlu.ca:/wwwhome/rmelnik/public_html/ammcs2015/
+python freezer.py
+
+files="";
+if [ "$1" = "--html" ]; then
+  echo "deploying html files only"
+  files="build/**/*.html";
+else
+  files="build/*"
+fi
+
+echo "Connecting to server..."
+scp -r $files lifc1350@cubic.wlu.ca:/wwwhome/rmelnik/public_html/ammcs2015/
