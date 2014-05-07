d9a237b4bb875347ebc0bd60c93cc6fe036aba12
George Lifchits <george.lifchits@gmail.com>
2014-05-07 18:19:02 -0400

    use rsync

--- site/deploy.sh
+++ site/deploy.sh
@@ -2,13 +2,5 @@
 
 python freezer.py
 
-files="";
-if [ "$1" = "--html" ]; then
-  echo "deploying html files only"
-  files="build/**/*.html";
-else
-  files="build/*"
-fi
-
 echo "Connecting to server..."
-scp -r $files lifc1350@cubic.wlu.ca:/wwwhome/rmelnik/public_html/ammcs2015/
+rsync -a --ignore-existing build/ lifc1350@cubic.wlu.ca:/wwwhome/rmelnik/public_html/ammcs2015/
