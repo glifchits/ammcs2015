d96d74687be69fbc1fa1c71ed179b2914509a720
George Lifchits <george.lifchits@gmail.com>
2014-06-10 16:37:56 -0400

    better deploy script

--- site/deploy.sh
+++ site/deploy.sh
@@ -3,5 +3,5 @@
 python freezer.py
 
 echo "Connecting to server..."
-scp -r build lifc1350@cubic.wlu.ca:/wwwhome/rmelnik/public_html/ammcs2015
-#rsync -a --ignore-existing build/ lifc1350@cubic.wlu.ca:/wwwhome/rmelnik/public_html/ammcs2015/
+#scp -r build/ lifc1350@cubic.wlu.ca:/wwwhome/rmelnik/public_html/ammcs2015
+rsync -avz --progress build/ lifc1350@cubic.wlu.ca:/wwwhome/rmelnik/public_html/ammcs2015/
