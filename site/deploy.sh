7fd04b341f66e10dd4f231790d1c0e176a2baf36
George Lifchits <george.lifchits@gmail.com>
2014-06-12 14:50:30 -0400

    google html file

--- site/deploy.sh
+++ site/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python freezer.py
-
+cp static/files/google82f44dcd8eab57e0.html build && echo "copied google validation html"
 echo "Connecting to server..."
 #scp -r build/ lifc1350@cubic.wlu.ca:/wwwhome/rmelnik/public_html/ammcs2015
 rsync -avz --progress build/ lifc1350@cubic.wlu.ca:/wwwhome/rmelnik/public_html/ammcs2015/
