c3d4fcadee508b863fa062fd8be6aac624908a66
George Lifchits <george.lifchits@gmail.com>
2014-06-19 13:15:11 -0400

    gulpfile with styles task and watching for scss

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -2,10 +2,14 @@ var gulp = require('gulp'),
 	watch = require('gulp-watch'),
 	sass = require('gulp-sass');
 
-gulp.task('default', function() {
+gulp.task('styles', function() {
 	gulp.src('static/scss/*.scss')
-		.pipe(watch(function(files) {
-			return files.pipe(sass())
-				.pipe(gulp.dest('static/css'));
-		}));
+		.pipe(sass())
+		.pipe(gulp.dest('static/css'));
+});
+
+gulp.task('default', ['styles'], function() {
+	gulp.watch('static/scss/*.scss', function(evt) {
+		gulp.run('styles');
+	});
 });
