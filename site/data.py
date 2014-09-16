333faea935ff44b1b49baf19418ac739e4871654
George Lifchits <george.lifchits@gmail.com>
2014-09-15 21:16:31 -0400

    big refactor, with global template variables in `data.py`
    added more up to date info for special sessions

---
+++ site/data.py
@@ -0,0 +1,81 @@
+#-*- coding: utf-8 -*-
+
+routes = (
+    ('Home', ''),
+    ('Special Sessions & Minisymposia', 'special-sessions'),
+    ('Contributed Sessions', 'contributed-sessions'),
+    ('Submit Abstracts', 'submit-abstracts'),
+    ('Deadlines & Payment', 'deadlines-payment'),
+    ('Plenary Speakers', 'plenary-speakers'),
+    ('Student Prize & Support', 'student-prize-support'),
+    ('Conference Program', 'conference-program'),
+    ('Proceedings', 'proceedings'),
+    ('Conference Flyer', 'conference-flyer'),
+    ('Travel & Local Info', 'travel-local-info'),
+    ('Venue & Accommodation', 'venue-accommodation'),
+    ('Organization', 'organization'),
+    ('Contact Us', 'contact-us')
+)
+
+sessions = (
+    ('scna',
+        'The 2nd Canadian Symposium on Scientific Computing and Numerical Analysis',
+        (
+            'Scott MacLachlan (MUN)', 'Justin Wan (Waterloo)',
+            'Hans de Sterck (Waterloo)',
+            'Ben Adcock (SFU – currently moving from Purdue)'
+        )
+        )
+    ,
+    ('im',
+        'Industrial Mathematics',
+        (
+            'Huaxiong Huang (York)',
+            'John Stockie (SFU)',
+            'Odile Marcotte (UQAM & CRM)',
+            'Sean Bohun (UOIT)',
+        )
+        )
+    ,
+    ('mb',
+        'Mathematical Biology', ('Frithjof Lutscher (Ottawa)',)
+        )
+    ,
+    ('aads', 'Applied analysis and dynamical systems',
+        (
+            'Xingfu Zou (UWO)',
+            'Dmitry Pelinovsky (McMaster)',
+            'David Iron (Dalhousie)'
+        )
+        )
+    ,
+    ('acm', 'Applied and computational mechanics',
+        ('Marek Stasna (Waterloo)', 'Bartek Protas (McMaster)')
+        )
+    ,
+)
+
+sess_by_code = { code: (info, org) for code, info, org in sessions }
+
+class committee:
+    scientific = (
+        ('Carlos', 'Garcia-Cevera', 'University of California, Santa Barbara'),
+        ('David', 'Cai', 'New York University'),
+        ('Dimitri', 'Vvedensky', 'Imperial College London'),
+        ('Ian', 'Frigaard', 'University of British Columbia'),
+        ('John', 'Lowengrub', 'University of California, Irvine'),
+        ('Nicholas', 'Zabaras', 'University of Warwick'),
+        ('Raymond', 'Spiteri', 'University of Saskatchewan'),
+        ('Shaofan', 'Li', 'University of California, Berkeley'),
+        ('Chi-Wang', 'Shu', 'Brown University'),
+        ('Boris', 'Malomed', 'Tel Aviv University'),
+        ('Anatoli', 'Ivanov', 'Penn State University'),
+        ('Olof', 'Runborg', 'KTH Royal Institute of Technology'),
+        ('Alberto', 'Bressan', 'Penn State University'),
+    )
+
+    technical = (
+        ('George', 'Lifchits', 'Web Coordinator'),
+        ('Sanjay', 'Prabhakar', 'Computer Support'),
+        ('Shyam', 'Badu', 'Digital Media Support')
+    )
--- site/main.py
+++ site/main.py
@@ -1,24 +1,56 @@
-import os
+# -*- coding: utf-8 -*-
 
+# builtin
+import os
+from collections import OrderedDict
+# library
 from flask import Flask, render_template, abort, redirect
-from config_pages import PAGE_FOLDER, PAGES, ROUTES, SESSIONS
+from data import routes, sessions
+# modules
+import data
+
+HTML = '.html'
+PAGE_FOLDER = 'pages'
 
 app = Flask(__name__, template_folder=PAGE_FOLDER)
 
-HTML = '.html'
+
+@app.template_filter('natural_list')
+def natural_list(lst):
+    def to_str(e):
+        return e.decode('utf-8')
+    lst = map(to_str, lst)
+    return ', '.join(lst)
+
+# determines the set of accessible pages
+PAGES = set()
+for root, directory, files in os.walk(PAGE_FOLDER):
+    for page in files:
+        if page.startswith('_'): continue
+        fullpath = os.path.join(root, page)[len(PAGE_FOLDER)+1:]
+        namedpath = os.path.splitext(fullpath)[0]
+        PAGES.add(namedpath)
+
+def data_obj():
+    '''Get a dictionary of all attributes in the `data` module'''
+    keys = [ k for k in dir(data) if not k.startswith('_') ]
+    return { k: eval('data.'+k) for k in keys }
+
 
 @app.route('/')
 def main():
     return render_template('index.html', pages=ROUTES)
 
+
 @app.route('/<path:route>')
 @app.route('/<path:route>/')
 def routing(route):
     if route in PAGES:
-        return render_template(route+HTML,
-            pages = ROUTES,
-            sessions = SESSIONS
-        )
+        obj = data_obj()
+        ROUTES = OrderedDict()
+        for k, v in routes:
+            ROUTES[k] = v
+        return render_template(route+HTML, pages=ROUTES, **obj)
     else:
         abort(404)
 
