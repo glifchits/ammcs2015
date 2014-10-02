c19cdafb0b9a61026b68563f3e277e5a992dee07
George Lifchits <george.lifchits@gmail.com>
2014-10-02 18:39:22 -0400

    refactored special sessions into new file, added some ss descriptions

--- site/data.py
+++ site/data.py
@@ -17,62 +17,7 @@ routes = (
     ('Contact Us', 'contact-us')
 )
 
-
-sessions = [
-    ('ddd', 'ss', 'Title: TBA', (
-            'Elena Braverman (University of Calgary)',
-            'Anatoli Ivanov (Penn State University)'
-        )
-    ),
-    ('hs', 'ss', 'Title: TBA', (
-            'Xinzhi Liu (University of Waterloo)',
-        )
-    ),
-    ('msmb', 'ss', 'Modeling & Simulation in Medicine and Biology', (
-            'Suzanne Shontz (University of Kansas)',
-            'Corina Drapaca (Penn State University)'
-        )
-    ),
-    ('mmnn', 'ss', 'Mathematical Models for Nanoscience and Nanotechnology',
-        ( 'Zoran Miskovic and Hamed Majedi (University of Waterloo)',)
-    ),
-    ('aaip', 'ss', 'Title TBA',
-        ( 'Herb Kunze (University of Guelph)', )
-    ),
-    ('sndta', 'ss', 'Title TBA',
-        ( 'Manuele Santoprete (WLU, Waterloo)', )
-    ),
-    ('scna', 'st', 'The 2nd Canadian Symposium on Scientific Computing and Numerical Analysis', (
-            'Scott MacLachlan (MUN)',
-            'Justin Wan (Waterloo)',
-            'Hans de Sterck (Waterloo)',
-            'Ben Adcock (SFU)'
-        )
-    ),
-    ('im', 'st', 'Industrial Mathematics', (
-            'Huaxiong Huang (York)',
-            'John Stockie (SFU)',
-            'Odile Marcotte (UQAM & CRM)',
-            'Sean Bohun (UOIT)',
-        )
-    ),
-    ('mb', 'st', 'Mathematical Biology', (
-            'Frithjof Lutscher (Ottawa)',
-            'Lea Popovic (Concordia)'
-        )
-    ),
-    ('aads', 'st', 'Applied analysis and dynamical systems', (
-            'Xingfu Zou (UWO)',
-            'Dmitry Pelinovsky (McMaster)',
-            'David Iron (Dalhousie)'
-        )
-    ),
-    ('acm', 'st', 'Applied and computational mechanics', (
-            'Marek Stasna (Waterloo)',
-            'Bartek Protas (McMaster)'
-        )
-    ),
-]
+from special_sessions import sessions
 
 sessions.sort(key = lambda sess: sess[0]) # sort by code
 sessions.sort(key = lambda sess: sess[1]) # sort by type (SS, ST)
