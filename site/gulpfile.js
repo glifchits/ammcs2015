0ac8a859c2fa3a3699f5673983f9b71bca69efab
George Lifchits <george.lifchits@gmail.com>
2014-11-16 14:28:03 -0500

    better gulpfile for better dev process

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp'),
 	watch = require('gulp-watch'),
 	sass = require('gulp-sass'),
 	imageResize = require('gulp-image-resize'),
-	rename = require('gulp-rename');
+	rename = require('gulp-rename'),
+	run = require('gulp-run'),
+	connect = require('gulp-connect');
 
 
 gulp.task('images', function() {
@@ -21,8 +23,23 @@ gulp.task('styles', function() {
 		.pipe(gulp.dest('static/css'));
 });
 
-gulp.task('default', ['styles', 'images'], function() {
+gulp.task('freeze', function() {
+	run('python ./freezer.py').exec();
+});
+
+gulp.task('serve', function() {
+	connect.server({
+		root: 'build',
+		livereload: true,
+		port: 5000
+	});
+});
+
+gulp.task('default', ['styles', 'images', 'freeze', 'serve'], function() {
 	gulp.watch('static/scss/*.scss', function(evt) {
 		gulp.run('styles');
+	});
+	gulp.watch(['**/*.py', 'pages/**/*.html'], function() {
+		gulp.run('freeze');
 	});
 });
