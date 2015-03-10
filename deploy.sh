de50d78ac5a8d2218ad9ef2d0f74f9ef9e7bd741
George Lifchits <george.lifchits@gmail.com>
2015-03-10 09:27:02 -0400

    chmod on build

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 gulp styles
 python freezer.py
 cp static/files/google82f44dcd8eab57e0.html build && echo "copied google validation html"
+chmod -R 775 build/
 echo "Connecting to server..."
 #scp -r build/ lifc1350@cubic.wlu.ca:/wwwhome/rmelnik/public_html/ammcs2015
 rsync -avz --progress build/ lifc1350@cubic.wlu.ca:/wwwhome/rmelnik/public_html/ammcs2015/
