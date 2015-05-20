25134c9222c024abeff0200064c6bef1344e60fc
George Lifchits <george.lifchits@gmail.com>
2015-05-20 12:40:34 -0700

    add robots.txt and deploy instruction

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 gulp styles
 python freezer.py
 cp static/files/google82f44dcd8eab57e0.html build && echo "copied google validation html"
+cp static/files/robots.txt build && echo "copied robots.txt"
 chmod -R 775 build/
 echo "Connecting to server..."
 #scp -r build/ lifc1350@cubic.wlu.ca:/wwwhome/rmelnik/public_html/ammcs2015
